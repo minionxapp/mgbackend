3109824b8d15b001db56962648439777c5006824
YUNI MUGIARTO <yuni.mugiarto@pegadaian.co.id>
2021-12-17 13:28:21 +0700

    perbaiki url test api

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,18 +9,22 @@ class TestController extends Controller
     public function index()
     {
         //Login
-        $request = Request::create('https://mgbackend.herokuapp.com/api/login', 'POST');
+        $url_login ='https://mgbackend.herokuapp.com/api/login';
+        // $url_login ='http://127.0.0.1:8000/api/login';
+
+        $request = Request::create($url_login, 'POST');
         $request->headers->set('Accept', 'application/json');
         // $request->headers->set('Authorization', 'Bearer '.$token);
         $request->merge(['email' => 'mugi@gmail.com', 'password' => '1234qwerty']);
         $res = app()->handle($request);
         $profile_details = json_decode($res->getContent()); // convert to json object
         // return $profile_details;
-        dd($profile_details);
         $token = $profile_details->access_token;
 
 
-        $request2 = Request::create('https://mgbackend.herokuapp.com/api/programs', 'GET');
+        $url_prog ='https://mgbackend.herokuapp.com/api/api/programs';
+        // $url_prog ='http://127.0.0.1:8000/api/programs';
+        $request2 = Request::create($url_prog, 'GET');
         $request2->headers->set('Accept', 'application/json');
         $request2->headers->set('Authorization', 'Bearer '.$token);
         $res2 = app()->handle($request2);
